f644d4aec53167d56ed891a0ca50c275ed0e6b6f
Mateusz Przybysz <notentego678@gmaill.com>
2021-05-06 18:01:59 +0200

    Project configuration

---
+++ src/test/java/pl/maprzybysz/springboot2security/SpringBoot2SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maprzybysz.springboot2security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
